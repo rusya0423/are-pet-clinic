938d0f1e634c384a850672ebbbb87ea32ce67240
Rustam Assylkhan <rusya0423@gmail.com>
2020-06-18 02:11:57 +0600

    add ARE Pet Clinic Project

---
+++ src/main/java/are/springframework/arepetclinic/ArePetClinicApplication.java
@@ -0,0 +1,13 @@
+package are.springframework.arepetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArePetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArePetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/are/springframework/arepetclinic/ArePetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package are.springframework.arepetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArePetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
